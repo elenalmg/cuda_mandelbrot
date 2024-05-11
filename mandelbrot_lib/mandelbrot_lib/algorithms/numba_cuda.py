419c9ad56a6fae457e9866ecfd6bd99a5cd71114
eloumagne <elena.loumagne@ensae.fr>
2024-05-11 19:15:50 +0000

    cleaning

--- mandelbrot_lib/mandelbrot_lib/algorithms/numba_cuda.py
+++ mandelbrot_lib/mandelbrot_lib/algorithms/numba_cuda.py
@@ -1,8 +1,5 @@
-from matplotlib.pyplot import imshow, show
-from timeit import default_timer as timer
 import numpy as np
 from numba import cuda
-from numba import *
 from ._base import _VectorBaseAlgorithm
 
 
@@ -29,7 +26,7 @@ class NumbaCuda(_VectorBaseAlgorithm):
         blockspergrid_y = (height + nthreads - 1) // nthreads
 
         @cuda.jit
-        def compute_grid_kernel(self, x_min: float, y_min: float, x_max: float, y_max: float, width: int, height: int, max_iter: int, results: np.ndarray):
+        def compute_grid_kernel(x_min: float, y_min: float, x_max: float, y_max: float, width: int, height: int, max_iter: int, results: np.ndarray):
             pixel_size_x = (x_max - x_min) / width
             pixel_size_y = (y_max - y_min) / height
             x, y = cuda.grid(2)
@@ -38,7 +35,7 @@ class NumbaCuda(_VectorBaseAlgorithm):
                 imag = y_min + y * pixel_size_y
                 results[y, x] = self.compute_escape_iter(real, imag, max_iter)
 
-        compute_grid_kernel[(blockspergrid_x, blockspergrid_y) , (nthreads, nthreads)](x_min, y_min, x_max, y_max, width, height, max_iter, results)
+        compute_grid_kernel[(blockspergrid_x, blockspergrid_y), (nthreads, nthreads)](x_min, y_min, x_max, y_max, width, height, max_iter, results)
         return results
 
 
