18499cc9a35993c3129659d5196078151656a121
Thomas Kientz <thomas@kientz.net>
2024-05-17 12:53:39 +0200

    Add wrapper

--- src/mandelbrot_cuda.cu
+++ src/mandelbrot_cuda.cu
@@ -23,22 +23,27 @@ __global__ void mandelbrot_kernel(float x_min, float y_min, float x_max, float y
     }
 }
 
+extern "C" void compute_grid_cuda(float x_min, float y_min, float x_max, float y_max, int width, int height, int max_iter, float escape_radius_squared, int* results) {
+    int* d_results;
+    size_t size = width * height * sizeof(int);
+    cudaMalloc(&d_results, size);
+    
+    dim3 threads_per_block(16, 16);
+    dim3 num_blocks((width + threads_per_block.x - 1) / threads_per_block.x, (height + threads_per_block.y - 1) / threads_per_block.y);
+    
+    mandelbrot_kernel<<<num_blocks, threads_per_block>>>(x_min, y_min, x_max, y_max, width, height, max_iter, escape_radius_squared, d_results);
+    
+    cudaMemcpy(results, d_results, size, cudaMemcpyDeviceToHost);
+    cudaFree(d_results);
+}
+
 class MandelbrotCUDA {
 public:
     MandelbrotCUDA(float escape_radius) : escape_radius_squared(escape_radius * escape_radius) {}
 
     void compute_grid(float x_min, float y_min, float x_max, float y_max, int width, int height, int max_iter, std::vector<int>& results) const {
-        int* d_results;
-        size_t size = width * height * sizeof(int);
         results.resize(width * height);
-
-        cudaMalloc(&d_results, size);
-        dim3 threads_per_block(16, 16);
-        dim3 num_blocks((width + threads_per_block.x - 1) / threads_per_block.x, (height + threads_per_block.y - 1) / threads_per_block.y);
-        
-        mandelbrot_kernel<<<num_blocks, threads_per_block>>>(x_min, y_min, x_max, y_max, width, height, max_iter, escape_radius_squared, d_results);
-        cudaMemcpy(results.data(), d_results, size, cudaMemcpyDeviceToHost);
-        cudaFree(d_results);
+        compute_grid_cuda(x_min, y_min, x_max, y_max, width, height, max_iter, escape_radius_squared, results.data());
     }
 
 private:
